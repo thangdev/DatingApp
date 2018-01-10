81dec4bc31a23b56fef4d570358c01948190d97d
Khanh Nguyen <khanhnguyen@MacBook.local>
2018-01-11 01:53:17 +0700

    comment code  nam

--- app/src/main/java/com/example/nam_t/datingapp/MainActivity.java
+++ app/src/main/java/com/example/nam_t/datingapp/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+//        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 //
 //        // Set up the ViewPager with the sections adapter.
 //        mViewPager = (ViewPager) findViewById(R.id.container);
