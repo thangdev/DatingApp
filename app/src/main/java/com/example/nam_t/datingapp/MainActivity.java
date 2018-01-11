ce1058462cfa352ed60ff8b90b3a907424ee7129
ThangnvPTIT <thangnv97ptit@gmail.com>
2018-01-11 16:39:10 +0700

    thangnv push

--- app/src/main/java/com/example/nam_t/datingapp/MainActivity.java
+++ app/src/main/java/com/example/nam_t/datingapp/MainActivity.java
@@ -1,9 +1,23 @@
 package com.example.nam_t.datingapp;
 
-import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,16 +29,17 @@ public class MainActivity extends AppCompatActivity {
      * may be best to switch to a
      * {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
-//    private SectionsPagerAdapter mSectionsPagerAdapter;
+   // private SectionsPagerAdapter mSectionsPagerAdapter;?
 
     /**
      * The {@link ViewPager} that will host the section contents.
      */
-//    private ViewPager mViewPager;
+    //private ViewPager mViewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.conversation);
+        setContentView(R.layout.activity_main);
 
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
@@ -51,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-//    @Override
+    //@Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
 //        // Inflate the menu; this adds items to the action bar if it is present.
 //        getMenuInflater().inflate(R.menu.menu_main, menu);
