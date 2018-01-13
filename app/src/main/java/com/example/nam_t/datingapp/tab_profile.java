ad8db8530de5057bad497a00e5f2053ca467efc1
Khanh Nguyen <khanhnguyen@MacBook.local>
2018-01-13 20:59:47 +0700

    fuck

--- app/src/main/java/com/example/nam_t/datingapp/tab_profile.java
+++ app/src/main/java/com/example/nam_t/datingapp/tab_profile.java
@@ -6,6 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 /**
@@ -18,12 +23,33 @@ public class tab_profile extends Fragment {
         // Required empty public constructor
     }
 
+    private CircleImageView imgProfile;
+    private TextView txtNameProfile, txtDayProfile, txtMonthProfile, txtYearProfile, txtBioProfile;
+    private Button btnSave, btnLogout;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.current_user_profile, container, false);
+
+        View rootView = inflater.inflate(R.layout.current_user_profile, container, false);
+
+        txtNameProfile = (TextView) rootView.findViewById(R.id.txtNameProfile);
+
+        btnSave = (Button) rootView.findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Test", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        return rootView;
+
     }
 
 }
