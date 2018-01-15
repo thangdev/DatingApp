bfd84a59ca5fab47d7dfebbf2f392957fe70deca
ThangnvPTIT <thangnv97ptit@gmail.com>
2018-01-15 15:14:01 +0700

    done LoginActivity

--- app/src/main/java/com/example/nam_t/datingapp/LoginActivity.java
+++ app/src/main/java/com/example/nam_t/datingapp/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -53,14 +54,20 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 final String email = mEmail.getText().toString();
                 final String password = mPassword.getText().toString();
-                mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(!task.isSuccessful()){
-                            Toast.makeText(LoginActivity.this,"sign in error !!! ",Toast.LENGTH_LONG).show();
+                if(email.equals("") || password.equals("")) Toast.makeText(LoginActivity.this,"Fill the blank",Toast.LENGTH_LONG).show();
+
+                else {
+                    mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+
+
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(LoginActivity.this, "sign in error !!! ", Toast.LENGTH_LONG).show();
+                            }
                         }
-                    }
-                });
+                    });
+                }
             }
         });
 
@@ -69,6 +76,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(intent);
+
             }
         });
 
